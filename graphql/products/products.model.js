4b9f7321c51dddf96303d66ed8ddac68be9a2078
gourav garg <garggourav012@gmail.com>
2024-03-12 16:05:52 +0530

    Filter products query using graphQL

--- graphql/products/products.model.js
+++ graphql/products/products.model.js
@@ -1,26 +1,40 @@
-const products = [
-    {
-        id:'redTape',
-        description: 'RedTape men shoes',
-        price: 1170,
-        reviews: [
-            {
-                comment: 'A good quality shoes which is good in this price range',
-                rating:4.7
-            }
-        ]
-    },
-    {
-        id:'blue jeans',
-        description: 'blue jeans for men',
-        price: 800
-    }
-]
+    const products = [
+        {
+            id:'redTape',
+            description: 'RedTape men shoes',
+            price: 1170,
+            reviews: [
+                {
+                    comment: 'A good quality shoes which is good in this price range',
+                    rating:4.7
+                }
+            ]
+        },
+        {
+            id:'blue jeans',
+            description: 'blue jeans for men',
+            price: 800
+        }
+    ]
 
 function getAllProducts(){
     return products
 }
 
+function getProductsByPrice(min,max){
+    return products.filter((product) => {
+        return product.price >= min && product.price <= max
+    })
+}
+
+function getProductsById(id){
+    return products.find((product) => {
+        return product.id === id
+    })
+}
+
 module.exports = {
     getAllProducts,
+    getProductsByPrice,
+    getProductsById,
 }
