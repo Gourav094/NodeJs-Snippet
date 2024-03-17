8f76d91ac0e96f0e2b45313b7e77ac536a12d063
gourav garg <garggourav012@gmail.com>
2024-03-17 13:37:14 +0530

    created room for multiple-players

--- sockets/pong-game/socket.js
+++ sockets/pong-game/socket.js
@@ -3,25 +3,32 @@ function listen(io) {
     const pongNamespace = io.of('/pong')
     pongNamespace.on('connection', (socket) => {
         console.log('player connected ', socket.id)
+        let room;
         socket.on('ready', () => {
+            room = 'room' + Math.floor(connectedPlayer/2)
+            socket.join(room)
+
             console.log('player is ready..')
+            
             connectedPlayer++
+
             if (connectedPlayer % 2 === 0) {
                 console.log('Lets start the game')
-                pongNamespace.emit('startGame', socket.id)
+                pongNamespace.in(room).emit('startGame', socket.id)
             }
         })
 
         socket.on('movePaddle', (position) => {
-            socket.broadcast.emit('movePaddle', position)
+            socket.to(room).emit('movePaddle', position)
         })
 
         socket.on('moveBall', (position) => {
-            socket.broadcast.emit('moveBall', position)
+            socket.to(room).emit('moveBall', position)
         })
 
         socket.on('disconnect', (reason) => {
             console.log(`Client ${socket.id} disconnected : `, reason)
+            socket.leave(room)
         })
     })
 }
