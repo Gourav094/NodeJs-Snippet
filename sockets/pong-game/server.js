5df498011239848a42bcdfaa1843b5304d38ca35
gourav garg <garggourav012@gmail.com>
2024-03-17 11:16:31 +0530

    Integrate express server & socker server on same port

--- sockets/pong-game/server.js
+++ sockets/pong-game/server.js
@@ -1,33 +1,16 @@
-const server = require('http').createServer()
-const io = require('socket.io')(server)
+const io = require('socket.io')
+const http = require('http')
+
+const api = require('./api')
+const httpServer = http.createServer(api)
+const socketServer = io(httpServer)
+
+const sockets = require('./socket')
+
 const PORT = 3000
 
-server.listen(PORT,() => {
+httpServer.listen(PORT,() => {
     console.log(`server running on ${PORT}`)
 })
 
-let connectedPlayer = 0
-
-io.on('connection',(socket) => {
-    console.log('player connected ',socket.id)
-    socket.on('ready',() => {
-        console.log('player is ready..')
-        connectedPlayer++
-        if(connectedPlayer % 2 === 0){
-            console.log('Lets start the game')
-            io.emit('startGame',socket.id)
-        }
-    })
-
-    socket.on('movePaddle',(position) => {
-        socket.broadcast.emit('movePaddle',position)
-    })
-
-    socket.on('moveBall',(position) => {
-        socket.broadcast.emit('moveBall',position)
-    })
-
-    socket.on('disconnect',(reason) => {
-        console.log(`Client ${socket.id} disconnected : `,reason)
-    })
-})
+sockets.listen(socketServer)
---
+++ sockets/pong-game/socket.js
@@ -0,0 +1,30 @@
+let connectedPlayer = 0
+function listen(io) {
+    io.on('connection', (socket) => {
+        console.log('player connected ', socket.id)
+        socket.on('ready', () => {
+            console.log('player is ready..')
+            connectedPlayer++
+            if (connectedPlayer % 2 === 0) {
+                console.log('Lets start the game')
+                io.emit('startGame', socket.id)
+            }
+        })
+
+        socket.on('movePaddle', (position) => {
+            socket.broadcast.emit('movePaddle', position)
+        })
+
+        socket.on('moveBall', (position) => {
+            socket.broadcast.emit('moveBall', position)
+        })
+
+        socket.on('disconnect', (reason) => {
+            console.log(`Client ${socket.id} disconnected : `, reason)
+        })
+    })
+}
+
+module.exports = {
+    listen,
+}
