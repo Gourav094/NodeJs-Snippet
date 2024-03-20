b082ab03f176d35e439b97fb2aea81e0428e68b0
gourav garg <garggourav012@gmail.com>
2024-03-20 14:13:55 +0530

    autherization using passport and OAuth flow

--- node-security/server.js
+++ node-security/server.js
@@ -3,49 +3,80 @@ const https = require('https')
 const express = require('express')
 const helmet = require('helmet')
 const path = require('path')
+const passport = require('passport')
+const { Strategy } = require('passport-google-oauth20')
 require('dotenv').config()
 const app = express()
 
 const PORT = 3000
 
 const config = {
-    CLIENT_ID : process.env.CLIENT_ID,
-    CLIENT_SERVER:process.env.CLIENT_SERVER
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET
 }
+
+const AUTH_OPTIONS = {
+    callbackURL: '/auth/google/callback',
+    clientID: config.CLIENT_ID,
+    clientSecret: config.CLIENT_SECRET
+}
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+    console.log('Goggle take us back after authentication',profile)
+    done(null, profile)
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback))
 
 app.use(helmet())
 
-function checkLogin(req,res,next){
+app.use(passport.initialize())
+
+function checkLogin(req, res, next) {
     const Login = true
-    if(!Login){
+    if (!Login) {
         return res.status(401).json({
-            error:"You need to login !!"
+            error: "You need to login !!"
         })
     }
     next()
 }
 
 // for redirect to login
-app.get('/auth/google',(req,res) => {} )
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['email']
+}))
 
 //getting redirect callback for auth server
-app.get('/auth/google/callback',(req,res) => {} )
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect: '/',
+        failureRedirect: '/failure',
+        session: false
+    }),
+    (req, res) => {
+        console.log('authorization successfully done by google')
+    })
 
 //for logout
-app.get('/auth/logout',(req,res) => {} )
+app.get('/auth/logout', (req, res) => { })
 
-app.get('/secret',checkLogin,(req,res) => {
+app.get('/secret', checkLogin, (req, res) => {
     res.send('Your secret value is 2003')
 })
 
-app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname,'public','index.html'))
+app.get('/failure', (req, res) => {
+    res.send('Getting error in log in. Please try again later')
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
 https.createServer({
-    key:fs.readFileSync('key.pem'),
-    cert:fs.readFileSync('cert.pem')
-},app).listen(PORT,() => {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+}, app).listen(PORT, () => {
     console.log(`server running on https://localhost:3000`)
 })
 
